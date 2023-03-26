69a0bdbab339374012f763a1935eb62e6272af01
TomeT187 <tomet187@gmail.com>
2023-03-26 01:13:40 -0700

    first draft

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-from mainMenu import mainMenu
-
-#Are ya ready Kids?
+#
+#Read Readme first if cannot run the code
+#
+#This is the first file in order
+#
 #Order to read files
 #main.py -> mainMeny.py -> Calender.py -> Day.py -> dayArray.py -> dataClass.py 
+
+from mainMenu import mainMenu
+
+#Run this file
 if __name__ == "__main__":
     MainMenu = mainMenu()
     MainMenu.mainloop()
